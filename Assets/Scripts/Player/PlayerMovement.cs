e96ed8d0890eb959e527f97c8a28779831ae498a
Fouad Elamoury <foad.osama@hotmail.com>
2025-07-16 23:18:05 +0300

    fixed infinite jumping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,68 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	public CharacterController2D controller;
-	public Animator animator;
+    public CharacterController2D controller;
+    public Animator animator;
 
-	public float runSpeed = 40f;
+    public float runSpeed = 40f;
 
-	float horizontalMove = 0f;
-	bool jump = false;
-	bool dash = false;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool dash = false;
 
-	//bool dashAxis = false;
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			jump = true;
-			Debug.Log("Jump pressed");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+            Debug.Log("Jump pressed");
         }
 
-		if (Input.GetKeyDown(KeyCode.C))
-		{
-			dash = true;
-		}
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            dash = true;
+        }
+    }
 
-		/*if (Input.GetAxisRaw("Dash") == 1 || Input.GetAxisRaw("Dash") == -1) //RT in Unity 2017 = -1, RT in Unity 2019 = 1
-		{
-			if (dashAxis == false)
-			{
-				dashAxis = true;
-				dash = true;
-			}
-		}
-		else
-		{
-			dashAxis = false;
-		}
-		*/
+    public void OnFall()
+    {
+        // This is called when the player starts falling (transitions from grounded to not grounded)
+        // Don't set IsJumping here - it should only be set when actually jumping
+        Debug.Log("Player is falling");
+    }
 
-	}
+    public void OnLanding()
+    {
+        // This is called when the player lands (transitions from not grounded to grounded)
+        // Animation resets are handled in CharacterController2D
+        Debug.Log("Player landed");
+    }
 
-	public void OnFall()
-	{
-		animator.SetBool("IsJumping", true);
-	}
-
-	public void OnLanding()
-	{
-		animator.SetBool("IsJumping", false);
-	}
-
-	void FixedUpdate ()
-	{
-		// Move our character
-		controller.Move(horizontalMove * Time.fixedDeltaTime, jump, dash);
-		jump = false;
-		dash = false;
-	}
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump, dash);
+        jump = false;
+        dash = false;
+    }
 }
