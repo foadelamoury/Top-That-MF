e1a92da0f572306d35862aec8727c9ed1142ed38
Fouad Elamoury <foad.osama@hotmail.com>
2025-09-15 16:30:08 +0300

    fixing Main menu and adding timer
    -Fixing the icons missing due to references in controls menu
    - Adding Timer as an additive scene
    - Adding ground layer to in-destructive Game objects

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -21,7 +21,11 @@ public class MenuManager : MonoBehaviour {
     }
 
     // ---- internal handlers ----
-    void StartGame() => SceneManager.LoadSceneAsync(1);
+    void StartGame() {
+        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadScene("Timing", LoadSceneMode.Additive);
+        Debug.Log("The Timing should be added");
+    }
 
     void OpenSettingsMenuHandle() {
         if (mainMenuCanvasGO) mainMenuCanvasGO.SetActive(false);
